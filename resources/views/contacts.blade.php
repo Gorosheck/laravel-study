f1200804d8f176d446940d296e3ab35ed74a896a
Kirill Goroshko <gorosheck@gmail.com>
2022-08-23 13:33:11 +0300

    HW 17 done

---
+++ resources/views/contacts.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('title-block')Контакты@endsection
+
+@section('content')
+<h1>Контакты</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam cum eaque eligendi eveniet facilis hic ipsam quae similique. Adipisci aperiam aut cumque doloremque eaque explicabo incidunt omnis perferendis vel vero.</p>
+@endsection
+
+@section('contact-form')
+    <h2>Форма обратной связи</h2>
+
+    <form>
+        <div class="input-group input-group-lg mb-3">
+            <span class="input-group-text" id="inputGroup-sizing-lg">Name</span>
+            <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
+        </div>
+        <div class="input-group input-group-lg mb-3">
+            <span class="input-group-text" id="inputGroup-sizing-lg">E-mail</span>
+            <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
+        </div>
+        <div class="input-group input-group-lg mb-3">
+            <span class="input-group-text" id="inputGroup-sizing-lg">Tel</span>
+            <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    <title>@yield('title-block')</title>
+</head>
+<body>
+<div class="container">
+    <header class="d-flex justify-content-center py-3">
+        <ul class="nav nav-pills">
+            <li class="nav-item"><a href="/" class="nav-link active" aria-current="page">Главная</a></li>
+            <li class="nav-item"><a href="about" class="nav-link">О компании</a></li>
+            <li class="nav-item"><a href="contacts" class="nav-link">Контакты</a></li>
+        </ul>
+    </header>
+</div>
+@yield('content')
+
+@yield('content-block-home')
+
+@yield('contact-form')
+</body>
+</html>
